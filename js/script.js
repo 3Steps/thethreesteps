44c7ad508d89bae92ea4745952005325a18870c9
AishaLawal <noBeast21@mail.com>
2017-07-22 17:57:09 +0100

    basically done just colours left

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 var button = document.getElementById("more");
 var modal = document.getElementById("modal");
 var closeBtn = document.getElementById("closer");
+var entersite = document.getElementById("enter")
+var entersiteWindow = document.getElementById("window");
+entersite.onclick = function(){
+  $('#window').fadeOut("slow");
+};
+
 $('.txt').css('opacity','0');
 modal.style.display = "none"
 $('.total').addClass("animated");
