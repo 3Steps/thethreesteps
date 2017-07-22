e0db7cab212afd8c6fef93ca1d5dd5845f50cca2
AishaLawal <noBeast21@mail.com>
2017-07-22 15:19:33 +0100

    completed

--- js/script.js
+++ js/script.js
@@ -1,11 +1,32 @@
 var button = document.getElementById("more");
 var modal = document.getElementById("modal");
 var closeBtn = document.getElementById("closer");
-modal.style.display = "none";
+$('.txt').css('opacity','0');
+modal.style.display = "none"
+$('.total').addClass("animated");
+$('.total').addClass("rubberBand")
 
 button.onclick = function(){
   modal.style.display = "flex";
+  $('.box').addClass("animated");
+  $('.box').addClass("bounceIn");
+
 };
 closeBtn.onclick = function(){
   modal.style.display = "none";
+
+
+  $('.box').removeClass("animated");
+  $('.box').removeClass("bounceIn");
+
 }
+$(window).scroll(function(){
+  var scrl = $(this).scrollTop();
+
+
+  if($(".txt").offset().top < scrl + 700){
+      $(".txt").addClass("animated");
+      $('.txt').css('opacity','1');
+    $(".txt").addClass("bounceInRight");
+  }
+});
